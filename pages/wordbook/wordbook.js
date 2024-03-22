bbfa149a2523fc3884f81917991e966b1e99e4fb
pingzhu <pingzhu@tencent.com>
2024-03-22 11:53:43 +0800

    增加生词本展示

--- pages/wordbook/wordbook.js
+++ pages/wordbook/wordbook.js
@@ -1,15 +1,5 @@
 // pages/wordbook/wordbook.js
 
-/**获取云端book word, 云数据库 */
-async function getCloudWordList(bookid) {
- const db = wx.cloud.database()
- var openid=wx.getStorageSync('openid')
- console.log("*******查询条件****->",openid+"$$"+bookid)
- let data = await  db.collection('user_book_word').where({
-  '_openid':openid,'bookid':bookid}).get()
- return data.data
-}
-
 Page({
 
   /**
@@ -18,29 +8,13 @@ Page({
   data: {
     systemInfo:null,
     bookword:{},
-    title:'请先选择...'
+    title:'....'
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var bookid = wx.getStorageSync('bookid')
-    var bookword={}
-    var title='请先选择...'
-    if (bookid != '') {
-      bookword= getCloudWordList(bookid)
-      console.log('bookword ->', bookword)
-      if(bookword.length>0){
-        title=bookword['booktitle']
-      }else{
-        bookword={}
-      }
-    }
-    this.setData({
-      systemInfo:wx.getSystemInfoSync(),
-      title:title,
-      bookword:bookword
-    })
+  
   },
 
   /**
@@ -54,7 +28,27 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var bookid = wx.getStorageSync('bookid')
+    console.log("**********->",bookid)
+    var bookword={}
+    var title='请先选择....'
+    if (bookid != '') {
+        this.getCloudWordList(bookid).then(res =>{
+        if(res.length>0){
+          bookword=res[0]
+          title=bookword['booktitle']
+          this.setData({
+            systemInfo:wx.getSystemInfoSync(),
+            title:title,
+            bookword:bookword
+          })
+        }
+      })
+      .catch(err =>{
+        // .catch 返回报错信息
+        console.log(err)
+      })
+    }
   },
 
   /**
@@ -91,7 +85,14 @@ Page({
   onShareAppMessage: function () {
 
   },
-
+/**获取云端book word, 云数据库 */
+getCloudWordList:async function(bookid) {
+  const db = wx.cloud.database()
+  var openid=wx.getStorageSync('openid')
+  let data = await  db.collection('user_book_word').where({
+   '_openid':openid,'bookid':bookid}).get()
+  return data.data
+ },
   test:function(e){
     console.log('test ->',e)
   }
